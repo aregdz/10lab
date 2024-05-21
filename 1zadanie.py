e72cf4db29d2dd2aae044ad87b793349128d5948
aregdz <144221910+aregdz@users.noreply.github.com>
2024-05-21 13:52:33 +0300

    Update 1zadanie.py

--- 1zadanie.py
+++ 1zadanie.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# задание: Выполнить индивидуальное задание 1 лабораторной работы 4.1, максимально задействовав
+# задание: имеющиеся в Python средства перегрузки операторов.
+
+# Ошибки, которые были ранее: нужно выводить ошибку(e), убрать функцию make_pair(проверяет корректность введённых данных, ч
+# через исключения), данные исключения прописать в main
 
 class Pair:
     def __init__(self, first, second):
@@ -21,37 +26,25 @@ class Pair:
 
     def read(self):
         first = int(input("Введите целое положительное число для 'first': "))
-        second = float(
-            input("Введите дробное положительное число для 'second': ")
-        )
+        second = float(input("Введите дробное положительное число для 'second': "))
         self.set_first(first)
         self.set_second(second)
 
     def display(self):
-        print(
-            f"Калорийность 100 г продукта: {self.first} ккал,\nМасса продукта: {self.second} кг."
-        )
+        print(f"Калорийность 100 г продукта: {self.first} ккал,\nМасса продукта: {self.second} кг.")
 
     def power(self):
         return self.first * self.second * 10
-
-
-def make_pair(first, second):
-    try:
-        pair = Pair(first, second)
-        return pair
-    except ValueError as ve:
-        print(ve)
-        return None
 
 
 if __name__ == "__main__":
     try:
         first = int(input("Введите калорийность 100 г продукта: "))
         second = float(input("Введите массу продукта в килограммах: "))
-        my_pair = make_pair(first, second)
-        if my_pair:
-            my_pair.display()
-            print(f"Общая калорийность продукта: {my_pair.power()} ккал.")
+        my_pair = Pair(first, second)
+        my_pair.display()
+        print(f"Общая калорийность продукта: {my_pair.power()} ккал.")
+    except ValueError as ve:
+        print(f"Введены некорректные данные. {ve}")
     except Exception as e:
-        print("Введены некорректные данные.")
+        print(f"Произошла ошибка: {e}")
